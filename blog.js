83c09df0da9d69ea2eb20979c2ae362213f1d061
Jeff Lin <rslin@ucsd.edu>
2023-03-03 22:19:03 -0800

    js overhaul and part 3 done

--- blog.js
+++ blog.js
@@ -1,92 +1,18 @@
-let posts = []
-const addBtn = document.getElementById("addBtn");
-//const addPrompt = document.getElementById("addPrompt");
+const posts = JSON.parse(localStorage.getItem("blog-posts")) || [];
 
-const UISelector = {
-    titleInput: "#titleInput",
-    confirmBtn: "#trueConfirm",
-    dateInput: "#dateInput",
-    summaryInput: "#summaryInput",
-    promptDialog: "#addPrompt",
-    deletePrompt: "#delPrompt",
-    delTrue: "#delTrue",
-    addPrompt: "#addPrompt"
+export function addPost(newPost) {
+    posts.push(newPost);
+    localStorage.setItem("blog-posts", JSON.stringify(posts));
 }
 
-// showAddPrompt(addBtn, addPrompt);
-// function showAddPrompt(button, dialog) {
-addBtn.addEventListener('click', () => {
-    //console.log("call addBTN eventListener")
-    let addPrompt = document.querySelector(UISelector.promptDialog);
-    addPrompt.showModal();
-    //console.log("called Modal")
-    //let addPrompt = document.querySelector(UISelector.promptDialog);
-    let confirmBtn = document.querySelector(UISelector.confirmBtn);
-    addPrompt.addEventListener('close', (event) => {
-        console.log("closing..")
-        if (addPrompt.returnValue == confirmBtn.value) {
-            let post = retrievePost();
-            console.log(post)
-            addPost(post);
-        }
-    })
-});
-//}
-
-function retrievePost() {
-    let postDate = document.querySelector(UISelector.dateInput).value;
-    let postTitle = document.querySelector(UISelector.titleInput).value;
-    let postSummary = document.querySelector(UISelector.summaryInput).value;
-    let post = { postDate, postTitle, postSummary }
-    return post;
+export function editPost(newPost, index) {
+    posts[index] = newPost;
+    localStorage.setItem("blog-posts", JSON.stringify(posts));
 }
 
-function promptAlert() {
-    document.getElementById("emptyPrompt").showModal();
-
+export function deletePost(index) {
+    posts.splice(index, 1);
+    localStorage.setItem("blog-posts", JSON.stringify(posts));
 }
 
-function addPost(post) {
-    posts.push(post)
-    renderList();
-}
-
-//delete the post selected 
-function delPost(index) {
-    let delPrompt = document.querySelector(UISelector.deletePrompt)
-    let delTrue = document.querySelector(UISelector.delTrue)
-    delPrompt.showModal();
-    delPrompt.addEventListener('close', (event) => {
-        if (delPrompt.returnValue == delTrue.value) {
-            posts.splice(index, 1);
-            renderList();
-        }
-    })
-
-}
-
-function renderList() {
-    let postList = ""
-    for (let [index, post] of posts.entries()) {
-        if (post.postDate === '' || post.postTitle === '' || post.postSummary === '') {
-            continue;
-        }
-        postList += `
-        <li>
-            <span> ${post.postDate} </span>
-            <span> ${post.postTitle} </span>
-            <span> ${post.postSummary} </span>
-            <span class="edit" onclick="editPost(${index})">edit</span>
-            <span class="delete" onclick="delPost(${index})">delete</span>
-        </li>
-        `
-    }
-    document.querySelector("#list").innerHTML = postList;
-
-    //clear input box
-    document.querySelector(UISelector.dateInput).value = null;
-    document.querySelector(UISelector.titleInput).value = null;
-    document.querySelector(UISelector.summaryInput).value = null;
-}
-
-
+export default posts;
--- customdialog.js
+++ customdialog.js
@@ -66,4 +66,39 @@ export function customPrompt(message, onClose) {
     })
 }
 
-export function postPrompt(initialPost={})
+export function postPrompt(initialPost, onSave) {
+    let dialogMarkup = `
+    <dialog id="addPostPrompt">
+        <form method="dialog">
+            <label for="dateInput">Date:</label><br>
+            <input type="date" id="dateInput" name="dateInput" required /> <br>
+            <label for="titleInput">Title:</label><br>
+            <input type="text" id="titleInput" name="titleInput" required /> <br>
+            <label for="summaryInput">Summary:</label><br>
+            <input type="textarea" id="summaryInput" name="summaryInput" required /> <br>
+            <button value="save">Save</button>
+            <button value="cancel">Cancel</button>
+        </form>
+    </dialog>
+    `
+
+    setTimeout(() => {
+        document.body.insertAdjacentHTML('afterbegin', dialogMarkup);
+        let dialog = document.querySelector("dialog#addPostPrompt");
+        dialog.querySelector("#dateInput").value = initialPost.postDate;
+        dialog.querySelector("#titleInput").value = initialPost.postTitle;
+        dialog.querySelector("#summaryInput").value = initialPost.postSummary;
+        dialog.showModal();
+
+        dialog.addEventListener("close", () => {
+            if (dialog.returnValue === "save") {
+                onSave({
+                    postTitle: dialog.querySelector("#titleInput").value,
+                    postDate: dialog.querySelector("#dateInput").value,
+                    postSummary: dialog.querySelector("#summaryInput").value,
+                })
+            }
+            dialog.remove();
+        })
+    })
+}
